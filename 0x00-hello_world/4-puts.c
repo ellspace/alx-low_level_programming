98a25e61f06e269df70dd329acf42eb55b2fd72a
ellspace <a030.el@gmail.com>
2022-06-30 10:59:53 -0700

    "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main (void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
