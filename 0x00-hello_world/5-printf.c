7d6a8198de161e4869239d1ee03c2667be2b62c3
ellspace <a030.el@gmail.com>
2022-06-30 12:04:54 -0700

    new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry point
+ *main - Entry poin
  *
- * Return: Always 0 (success)
+ *Return: Always 0 (Success)
  */
 
-
 int main(void)
+
 {
 
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+		return (0);
 
 }
