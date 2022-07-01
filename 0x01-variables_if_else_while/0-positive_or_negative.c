5fa0796cc6c5e8d38ae2135aedf89802ecfabccc
ellspace <a030.el@gmail.com>
2022-07-01 09:10:48 -0700

    first today

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,20 @@
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
- *main - Entry point 
+ *main - Entry point
  *Return: Always 0 (success)
- *more headers goes there 
+ *more headers goes there
  */
 
 int main(void)
 
 {
+	int n;
 
-		int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					/* your code goes there */
-
-					return (0);
+	return (0);
 
 }
