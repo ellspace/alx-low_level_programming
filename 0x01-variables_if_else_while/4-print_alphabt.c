83879445a7649831d19ffa61280508285a16b3d8
ellspace <a030.el@gmail.com>
2022-07-01 13:32:11 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char h;
+
+	for (h = 'a'; h <= 'z'; h++)
+		if (h != 'e' && h != 'q')
+			putchar(h);
+	putchar('\n');
+
+	return (0);
+
+}
