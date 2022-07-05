76e4a78c6c424235c7212fb6d3d6ca4c1537daf9
ellspace <a030.el@gmail.com>
2022-07-05 05:37:01 -0700

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	putchar(n);
+
+	return (0);
+
+	}
+
+}
