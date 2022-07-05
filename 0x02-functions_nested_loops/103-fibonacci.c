b5e9a85e19f631d0db30f0a23281c9eeda696dda
ellspace <a030.el@gmail.com>
2022-07-05 14:17:40 -0700

    print fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h
+
+/**
+ * main - to print fiboncci
+ *
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	unsigned int x = 1, y = 2, z = 3, v = 2, s;
+
+	for (s = 2; s <= 32; s++)
+	{
+		if (z % 2 == 0)
+		{
+			v = v + z;
+			x = y;
+			y = z;
+			z = x + y;
+		}
+		printf("%u\n", v);
+		return (0);
+	}
+}
