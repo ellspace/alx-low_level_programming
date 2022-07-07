9c71515fd7c9ff46494bbf2280d9910200a463fe
ellspace <a030.el@gmail.com>
2022-07-07 08:36:39 -0700

    a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _isupper - checks wheather number is uppercase or lower case.
+ *
+ * @c: variable
+ *
+ * Return: 1 if c _isupper or 0 if otherwise
+ */
+
+
+int _isupper(int c)
+{
+
+	if (_isupper(c))
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more nested loops like i understand these are loops in other loop containers{{}}.
