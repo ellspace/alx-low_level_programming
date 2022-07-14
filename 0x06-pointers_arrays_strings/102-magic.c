6a925f6aa0db10fa9b2dd7ffd972355541bb1a90
ellspace <a030.el@gmail.com>
2022-07-14 15:14:19 -0700

    goddamit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
-
-
-/**
- * main - entry point
- *
- * Return: always (0)
- */
-
 int main(void)
 {
   int n;
@@ -14,9 +6,7 @@ int main(void)
   int *p;
   
   a[2] = 1024;
-  
   p = &n;
-
   /*
    * write your line of code here...
    * Remember:
