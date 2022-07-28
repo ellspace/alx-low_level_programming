195181ab0e30bb8f8866bfab4da82985cadd25c5
ellspace <a030.el@gmail.com>
2022-07-28 09:06:36 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define END '\0'
+#define EMPTY ""
+
+#include <string.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int _putchar(char c);
 
+void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
