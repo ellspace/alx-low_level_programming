6e2180f40765474f5f3365d86d41c34f4c90ddb1
ellspace <a030.el@gmail.com>
2022-08-03 13:45:13 -0700

    blody good economist david dddddddddiiiii

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,23 +1,53 @@
 #include "3-calc.h"
 
 
-
+/**
+ * op_add - addition
+ *
+ * @a: value
+ * @b: value
+ *
+ * Return: integer
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtraction
+ *
+ * @a: value
+ * @b: value
+ *
+ * Return: integer
+ */
 int op_sub(int a, int b)
 {
-
 	return (a - b);
 }
 
+/**
+ * op_mul - multiplication
+ *
+ * @a: value
+ * @b: value
+ *
+ * Return: integer
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - division
+ *
+ * @a: value
+ * @b: value
+ *
+ * Return: integer
+ */
 
 int op_div(int a, int b)
 {
@@ -26,10 +56,17 @@ int op_div(int a, int b)
 		ERR_OUT;
 		ERR_INF;
 	}
-
 	return (a / b);
-
 }
+
+/**
+ * op_mod - modulus
+ *
+ * @a: value
+ * @b: value
+ *
+ * Return: integer
+ */
 
 int op_mod(int a, int b)
 {
